24fe48fe15414406780135079e051d8803f3af46
Ruslan <ruslan.postoyuk@gmail.com>
2022-04-08 22:20:13 +0200

    165. Redux-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,28 @@
+/* Мы здесь передаём request потому что он будет делать запрос по этому адрессу "http://localhost:3001/heroes" а эта 
+сущность request у нас хранится в отдельном хуке 
+   Здесь уже не dispatch(heroesFetching), а dispatch(heroesFetching()). Потому что когда мы запускам dispatch перед
+ззапросом, то мы здесь устанавливаем индикатор Загрузки
+    Дальше когда мы уже сделали запрос то мы вызываем другой екшенКриейтор heroesFetched с теми данными которые мы
+получили от сервера (data) 
+    Дальше эти данные мы передаем в качестве полезной нагрузки в наш reducer в Екшене heroesFetched в payload: heroes 
+а туда они приходят в качестве аргумента (heroes) 
+    Если будет ошибка то туда передается еще один ЕкшенКрейтор
+    То-есть теперь у нас есть комплексный екшенКрейтор, который будет делать все за нас, он будет получать наших
+героев и обрабатывать различные состояния*/
+export const fetchHeroes = (request) => (dispatch) => {
+    dispatch(heroesFetching())
+    request("http://localhost:3001/heroes")
+        .then(data => dispatch(heroesFetched(data)))
+        .catch(() => dispatch(heroesFetchingError()))
+}
+
+export const fetchFilters = (request) => (dispatch) => {
+    dispatch(filtersFetching());
+    request("http://localhost:3001/filters")
+        .then(data => dispatch(filtersFetched(data)))
+        .catch(() => dispatch(filtersFetchingError()))
+}
+
 export const heroesFetching = () => {
     return {
         type: 'HEROES_FETCHING'
@@ -36,11 +61,31 @@ export const filtersFetchingError = () => {
     }
 }
 
-export const activeFilterChanged = (filter) => {
-    return {
+/* Так как мы подключили новый Middleware мы знаем что теперь мы сможем туда передать не только Объект(который возвращается
+из этого екшенКрейтора) но и функцию
+    Мы дописываем => (dispatch) =>
+    То-есть теперь когда у нас вызывается екшенКрейтор(вот эта часть const activeFilterChanged = (filter) =>), то он
+будет возвращать нам функцию, которая в себя принимает dispatch(и делает что-то внутри себя)
+    Когда мы используем Thunk Middleware, то диспетч (dispatch) нам сюда приходит автоматически. Его не нужно не откуда
+импортировать ! Он сам сюда прийдет и подставит сюда Thunk и уже будет использовать внутри этой функци
+    Теперь нам нужно выполнить основную задачу, те взять и задиспетчить этотй Объект return {
         type: 'ACTIVE_FILTER_CHANGED',
         payload: filter
-    }
+    } спустя определенное количество времени
+    Bырызем полностью этот Объект и добавляем команду setTimeout
+    Bнутри так как нам приходит (dispatch) внутри этой функции в качестве аргумента. То мы можем вызвать dispatch,
+потом расскрыть его и во внутрь поместить тот Объект, который и будет являться нашим екшеном
+    То-есть теперь мы возвращаем функцию, которая через одну секунду, будет запускать нужный нам Диспетч
+    Происходит это из-за того что Middleware автоматически передаёт dispatch в возвращаемую функцию
+    Это открывает нам кучу возможностей для наших фантазий ведь в этой функции может происходить всё что угодно
+   */
+export const activeFilterChanged = (filter) => (dispatch) => {
+    setTimeout(() => {
+        dispatch({
+            type: 'ACTIVE_FILTER_CHANGED',
+            payload: filter
+        }) 
+    }, 500)
 }
 
 export const heroCreated = (hero) => {
--- src/store/index.js
+++ src/store/index.js
@@ -1,60 +1,11 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk'
 import heroes from '../reducers/heroes'
 import filters from '../reducers/filters';
 
-/* Если enhancer мог разшерять любую часть Стора, то middleware занимается именно фцией Диспетч(что почти всегда нам и 
-нужно) 
-    Фция stringMiddleware будет принимать в себя store
-    Эта фция будет возвращать другую фцию, которая автоматически будет подхватывать метод dispatch
-    А эта функция в свою очередь будет возвращать еще одну фцию, которая как аргумент будет принимать action и этот 
-action потом будет передаваться в dispatch
-    По-факту эта фция (action) => {} и есть новая фция dispatch которая имеет измененный функционал
-    if (typeof action === 'string') {
-        return dispatch({
-            type: action
-        })
-    }
-    return dispatch(action)
-    На этом всё, весь stringMiddleware уже работает. 
-    В enhacer нам сначало скопировать ссылку на старый Диспетч
-    потом перезаписать в сторе новую нашу фцию
-    и в итоге вернуть немножко измененный стор с другим диспетчем
-
-    Здесь же мы сразу возвращаем фцию dispatch с новый фционалом
-    Но нужно помнить что (store) расположен не весь Стор, а только фция Диспетч которая передается дальше и этом метод
-getState. То-есть это можно заменить на {dispatch, getState}
-    getState здесь был бы нужен если вдруг внутри этого Диспетча мы бы захотели получить какое-то текущее значение
-стейта и что-то с ним сделать
-    Но так как у нас не то и нето не используется то мы можем их убрать и оставить просто ()
-
-    Когда мы подключаем много Middleware одним за другим, то у нас идет цепочка их вызовов. Мы последовательно изменяем
-фции Диспетч для того что бы в конце получить такую большую мощную фцию. 
-    И когда запускается первый Диспетч те вот здесь dispatch({
-            type: action
-        }) или вот здесь dispatch(action)
-    то на самом деле вместо него будет запущенна АБСОЛЮТНА ВСЯ ФЦИЯ ОТ (action) ДО ПОСЛЕДУЮЩЕГО Middleware
-    Продублируем Middleware что бы было понятней
-    Когда у нас будет итог первого Middleware, те либо dispatch({
-            type: action
-        }) или вот здесь dispatch(action)
-    То слудующий Middleware который будет запускаться он возьмет вот эту функцию
-    (action) => {
-    if (typeof action === 'string') {
-        return dispatch({
-            type: action
-        })
-    }
-        return dispatch(action)
-    потому что из него она возвращается
-    И запустит вместо вот этой вот части в нашем первом Middleware !!! dispatch({
-            type: action
-        }) или вот здесь dispatch(action)
-    Те вместо тех двух Диспетчей будет вызываться следующая функция и в неё как раз таки будет приходить action из
-Второго Middleware потому что у нас либо Объект будет вызываться {type: action} либо Объект который приходит из-вне
-dispatch(action). В любом случае во второй Middleware попадает action, который уже что-то будет делать внутри
-    По-этому здесь dispatch называется next, и мы это сделаем, просто поменяет dispatch на next
-    Потому что вместо next будет вызываться следующая функция из Middleware
-    */
+/* В Redux-thunk мы можем диспетчить функции, а значит и асинхронные операции
+    npm i redux-thunk --save
+   */
 const stringMiddleware = () => (next) => (action) => {
     if (typeof action === 'string') {
         return next({
@@ -72,7 +23,7 @@ const stringMiddleware = () => (next) => (action) => {
     return dispatch(action)
 } */
 
-const enhancer = (createStore) => (...args) => {
+/* const enhancer = (createStore) => (...args) => {
     const store = createStore(...args)
 
     const oldDispatch = store.dispatch
@@ -85,26 +36,12 @@ const enhancer = (createStore) => (...args) => {
         return oldDispatch(action)
     }
     return store
-}
+} */
 
-/* Теперь нам нужен механизм который будет последовательно всё это делать, то-есть модифицировать наш dispatch
-   Для этого импортируе applyMiddleware
-   Нам функция compose сейчас вторым аргументом не нужна
-   Теперь второй аргумент это applyMiddleware и в него прокидываем ссылку на наш stringMiddleware
-   Что бы включить наш window с Девтулс, НЕ получится просто через запятую подставить в applyMiddleware, потому что
-включение нашего Девтулса - это не Middleware. Если мы так сделаем то мы увидем ошибку с next
-   Что бы решить проблему то воспользуемся compose
-   
-   ВЫВОД: Middleware это функции по добавляению функционала и изменению работы dispatch 
-   Чаще всего они позволяют в качестве action принимать не только Объекты но и строки, фции и тд
-   Это не только момент оптимизации, но и создание дополнительно фционала
-   Почти никогда их не понадобиться создавать в ручную ! Они почти все уже есть готовые 
-   Но теперь мы знаем как они устроены внутри ! 
-   */
 const store = createStore( 
                     combineReducers({heroes, filters}),
-                    compose(applyMiddleware(stringMiddleware),
-                    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+                    compose(applyMiddleware(ReduxThunk, stringMiddleware),
+                            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
                     )
                     /* compose(
                         enhancer,

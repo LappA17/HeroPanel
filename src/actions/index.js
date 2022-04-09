17f0a43de52437f3560b3fc64902960258dc7a9b
Ruslan <ruslan.postoyuk@gmail.com>
2022-04-09 22:57:26 +0200

    167. Redux Toolkit: createAction()

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,17 @@
+/* Мы уже понимаем что когда мы создаем екшенКрейторы, то у нас очень много одинакового кода
+   createAction решает эту проблему */
+import { createAction } from "@reduxjs/toolkit"
+
+/* Сделаем Комплексный actionCreator и передадим строку "ewerre" в .then(data => dispatch(heroesFetched(data, "ewerre")))
+   Заходим на страницу, получаем всех героев, но мы вообщем-то не видим нигде эту строку, потому что она просто
+игнорируется, а все данные которые у нас пришли переходят в payload. Часть функцоинала createAction делает за нас
+
+    Иногда приходится payload нужно создавать вручную. 
+    Здесь есть очень важное правило - стараться в reducer не передавать больше одного поля, все действие которое нужно
+выполнить нужно стараться выполнить в actioncREATORах. И уже потом когда данные готовые - передаем в редьюсер только
+одно значение(которое вообщем-то приходит в качестве payload) так наш код будет простым и удобным 
+
+    В функцию createAction всегда передавать только строки первым аргументом ! */
 export const fetchHeroes = (request) => (dispatch) => {
     dispatch(heroesFetching())
     request("http://localhost:3001/heroes")
@@ -12,18 +26,31 @@ export const fetchFilters = (request) => (dispatch) => {
         .catch(() => dispatch(filtersFetchingError()))
 }
 
-export const heroesFetching = () => {
+/* export const heroesFetching = () => {
     return {
         type: 'HEROES_FETCHING'
     }
-}
+} */
 
-export const heroesFetched = (heroes) => {
+// Так как у нас очень простой тип действие то просто подставляем 'HEROES_FETCHING'
+// Теперь он создается при помощи одной строки а не 4
+export const heroesFetching = createAction('HEROES_FETCHING')
+
+/* export const heroesFetched = (heroes) => {
     return {
         type: 'HEROES_FETCHED',
         payload: heroes
     }
-}
+} */
+
+/* Хоть мы и не передавали payload: heroes, а просто оставили createAction('HEROES_FETCHED') то всё всё равно работает
+   Все герои грузятся, страничка работает, хотя не было дополнительного передачи героев в пейлоде
+   Дело в том что когда мы используем createAction, аргумент который приходит в ЕкшенКрейтор, автоматически переходит
+в поле с названием payload. ТЕ вот эта вот часть у нас реализуется автоматически payload: heroes. По-этому в нашем
+примере все работает. 
+    Но если мы добавим дополнительные аргументы в вызов ЕкшенКрейтора, то они не будут никуда передаваться 
+    Поднимаему выше в fetchHeroes там где мы работали с сервером*/
+export const heroesFetched = createAction('HEROES_FETCHED')
 
 export const heroesFetchingError = () => {
     return {

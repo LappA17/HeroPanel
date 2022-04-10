b7b2b96d3a206a5f1a1122b56c544954487ff21f
Ruslan <ruslan.postoyuk@gmail.com>
2022-04-10 14:18:27 +0200

    168. Redux Toolkit: createReducer()

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,5 @@
-/* Мы уже понимаем что когда мы создаем екшенКрейторы, то у нас очень много одинакового кода
-   createAction решает эту проблему */
 import { createAction } from "@reduxjs/toolkit"
 
-/* Сделаем Комплексный actionCreator и передадим строку "ewerre" в .then(data => dispatch(heroesFetched(data, "ewerre")))
-   Заходим на страницу, получаем всех героев, но мы вообщем-то не видим нигде эту строку, потому что она просто
-игнорируется, а все данные которые у нас пришли переходят в payload. Часть функцоинала createAction делает за нас
-
-    Иногда приходится payload нужно создавать вручную. 
-    Здесь есть очень важное правило - стараться в reducer не передавать больше одного поля, все действие которое нужно
-выполнить нужно стараться выполнить в actioncREATORах. И уже потом когда данные готовые - передаем в редьюсер только
-одно значение(которое вообщем-то приходит в качестве payload) так наш код будет простым и удобным 
-
-    В функцию createAction всегда передавать только строки первым аргументом ! */
 export const fetchHeroes = (request) => (dispatch) => {
     dispatch(heroesFetching())
     request("http://localhost:3001/heroes")
@@ -26,20 +14,10 @@ export const fetchFilters = (request) => (dispatch) => {
         .catch(() => dispatch(filtersFetchingError()))
 }
 
+export const heroesFetching = createAction('HEROES_FETCHING')
 /* export const heroesFetching = () => {
     return {
         type: 'HEROES_FETCHING'
-    }
-} */
-
-// Так как у нас очень простой тип действие то просто подставляем 'HEROES_FETCHING'
-// Теперь он создается при помощи одной строки а не 4
-export const heroesFetching = createAction('HEROES_FETCHING')
-
-/* export const heroesFetched = (heroes) => {
-    return {
-        type: 'HEROES_FETCHED',
-        payload: heroes
     }
 } */
 
@@ -48,15 +26,21 @@ export const heroesFetching = createAction('HEROES_FETCHING')
    Дело в том что когда мы используем createAction, аргумент который приходит в ЕкшенКрейтор, автоматически переходит
 в поле с названием payload. ТЕ вот эта вот часть у нас реализуется автоматически payload: heroes. По-этому в нашем
 примере все работает. 
-    Но если мы добавим дополнительные аргументы в вызов ЕкшенКрейтора, то они не будут никуда передаваться 
-    Поднимаему выше в fetchHeroes там где мы работали с сервером*/
-export const heroesFetched = createAction('HEROES_FETCHED')
+    Но если мы добавим дополнительные аргументы в вызов ЕкшенКрейтора, то они не будут никуда передаваться */
+    export const heroesFetched = createAction('HEROES_FETCHED')
+/* export const heroesFetched = (heroes) => {
+    return {
+        type: 'HEROES_FETCHED',
+        payload: heroes
+    }
+} */
 
-export const heroesFetchingError = () => {
+export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR')
+/* export const heroesFetchingError = () => {
     return {
         type: 'HEROES_FETCHING_ERROR'
     }
-}
+} */
 
 export const filtersFetching = () => {
     return {
@@ -86,16 +70,19 @@ export const activeFilterChanged = (filter) => (dispatch) => {
     }, 500)
 }
 
-export const heroCreated = (hero) => {
+/* payload передается автоматически */
+export const heroCreated = createAction('HERO_CREATED')
+/* export const heroCreated = (hero) => {
     return {
         type: 'HERO_CREATED',
         payload: hero
     }
-}
+} */
 
-export const heroDeleted = (id) => {
+export const heroDeleted = createAction('HERO_DELETED')
+/* export const heroDeleted = (id) => {
     return {
         type: 'HERO_DELETED',
         payload: id
     }
-}
+} */
--- src/reducers/heroes.js
+++ src/reducers/heroes.js
@@ -1,9 +1,79 @@
+/* На данном этапе у нас много повторение кода в Редьюсере
+   Импортируем createRedeucer, и что бы его использовать нам понадобиться екшенКрейторы, которые и будут импортированы
+в этот Редьюсер. Делается это для того что бы эти функции мы могли подвязывать автоматически к этим Редьюсерам и в
+том числе избегать Копипасты, к примеру эти строки 'HEROES_FETCHING' мы написали в файлке actionCreator потом вставили 
+сюда в Редьюсер. И что бы этого не делать то мы сразу вставим сюда екшенКрейторы в наш reducer */
+import { createReducer } from "@reduxjs/toolkit"
+
+import {
+    heroesFetching,
+    heroesFetched,
+    heroesFetchingError,
+    heroCreated,
+    heroDeleted
+} from '../actions'
+
 const initialState = {
     heroes: [],
     heroesLoadingStatus: 'idle'
 }
 
-const heroes = (state = initialState, action) => {
+/* builder - Объект который повзволяет строить, конструировать Редьюсер при помощи Трёх Встроенных Методов 
+   Когда мы использует Туллкит то он автоматически использует библиотеку, под названием immer, а она в свою очередь занимается
+тем что упрощает работу с Иммутабильностью - это значит что мы можем писать фционал по прямому изменению Стейту, библиотека
+внутри сделает всё за нас
+    Раньше мы работали с Иммутабильностью так 
+    {
+        ...state,
+        heroesLoadingStatus: 'loading'
+    }
+    Мы создаем новый Объект, в котором разворачивали внутренности старого Объекта, и меняли в нем одно какое-то свойсвто
+    Если бы мы не меняли иммутабильность, то мы могли бы взять какой-то наш Стейт, обратиться к вот этому свойству
+heroesLoadingStatus и на прямую изменить его. Именно так мы можем делать когда используем createReducer
+    addCase - аналог case из switch case. Он принимает два аргумента: 1) ЕкшенКрейтор. 
+    2) Фцию по изменению Стейта - она принимает state и action, те тоже самое что у нас было вот здесь 
+const heroes = (state = initialState, action). ЗАМЕТЬ что эта функция ничего не возвращает, она просто запускается
+и внутри производит иммутацию, но если мы напишем return или напишем код в одну строчку(что бы код заретернился), то
+immer работать не будет, иммер подумает что разработчик сам позаботился о иммутабильность и себя не влкючит !!!
+
+    В heroesFetched у нас уже работа не просто со стейтом но и с Ешкеном
+    
+    В heroCreated мы писали непростой код heroes: [...state.heroes, action.payload] - мы создавали новый массив, 
+разварачивая старый массив и добавляли какой-то новый элемент(action.payload тут у нас лежит один Объект). 
+    Но так как мы можем теперь писать ммутабильный код, то в старый массив мы можем просто добавить в конец новый
+элемент - для этого воспользуемся push  
+
+    В onDelete, в state.heroes нужно поместить новый Массив, те не нужно создавать новый, а просто оставить все 
+элементы без того который был удален
+
+    Если мы прям так сохраним и зайдем на страничку, то будет ошибка, потому что при использование createReducer она 
+требует что бы наши команды были созданны при помощи createAction
+
+    Третий метод у builder - это addmatcher - позволяет фильтровать входящий action
+    */
+/* const heroes = createReducer(initialState, builder => {
+    builder //вызываем объект
+        .addCase(heroesFetching, state => {
+            state.heroesLoadingStatus = 'loading' // библиотека поймет что нужно сделать и будет соблюдать Иммутабильность
+        }) 
+        .addCase(heroesFetched, (state, action) => {
+            state.heroesLoadingStatus = 'idle'
+            state.heroes = action.payload
+        })
+        .addCase(heroesFetchingError, state => {
+            state.heroesLoadingStatus =  'error'
+        })
+        .addCase(heroCreated, (state, action) => {
+            state.heroes.push(action.payload)
+        })
+        .addCase(heroDeleted, (state,action) => {
+            state.heroes = state.heroes.filter(item => item.id !== action.payload)
+        })
+        .addDefaultCase(() => {}) //Это аналог Дефолта. Сюда обычно пишут пустую фцию, она будет вызываться если вдруг
+        //не найден action и наш Стейт останется точно таким же 
+}) */
+
+/* const heroes = (state = initialState, action) => {
     switch (action.type) {
         case 'HEROES_FETCHING':
             return {
@@ -33,6 +103,37 @@ const heroes = (state = initialState, action) => {
             }
         default: return state
     }
-}
+} */
 
+/* Так же есть второй вариант работы с createReducer, он работает только с ЖС, с ТайпКриптом работать не будет  
+   Вторым аргументом будет Объект вместо builder, где ключами будут actionCreator а их свойства - выполняемые действиеми
+   Здесь нужно будет пользоваться фишкой ЕС6 стандарта и будем динамически создавать ключи нашего Объекта
+   Выклядит это след образом - мы открываем [] и помещаем туда actionCreator и так как это объект пишем : и туда 
+помещаем фцию которая изменяет наш Стейт
+   Здесь работает тоже самое правило со словом return или писание кода в одну строчку state => state.heroesLoadingStatus = 'loading'
+   Что бы писать в одну строчку можно написать вот так state => {state.heroesLoadingStatus = 'loading'}
+   
+   В таком формате createReducer имеет три метода - ВТОРОЙ это массив сравнения - он нам не нужен по-этому осталяем
+пустой массив []
+   Третий - это как раз default, но здесь мы прописываем state => state */
+   const heroes = createReducer(initialState, {
+    [heroesFetching]: state => {state.heroesLoadingStatus = 'loading'},
+    [heroesFetched]: (state, action) => {
+                    state.heroesLoadingStatus = 'idle';
+                    state.heroes = action.payload;
+                },
+    [heroesFetchingError]: state => {
+                    state.heroesLoadingStatus = 'error';
+                },
+    [heroCreated]: (state, action) => {
+                    state.heroes.push(action.payload);
+                },
+    [heroDeleted]: (state, action) => {
+                    state.heroes = state.heroes.filter(item => item.id !== action.payload);
+                }
+        },
+    [],
+    state => state
+)
+            
 export default heroes;
